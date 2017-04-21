a9398affff0ce82cb1a07f2d4442170d7fd5b56b
Micheal <mrmyers2k@gmail.com>
2017-04-20 20:26:09 -0500

    Fixed debater comparing issues

--- src/io/micheal/debatescout/Debater.java
+++ src/io/micheal/debatescout/Debater.java
@@ -1,5 +1,8 @@
 package io.micheal.debatescout;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import io.micheal.debatescout.helpers.SQLHelper;
 
 public class Debater {
@@ -8,7 +11,7 @@ public class Debater {
 	private Integer id;
 	
 	public Debater(String name, String school) throws UnsupportedNameException {
-		name = name.replaceAll(" \\(drop\\)", "");
+		name = name.replaceAll(" \\(.+?\\)", "");
 		this.school = school;
 		String[] blocks = name.split(" ");
 		if(blocks.length == 0)
@@ -17,7 +20,7 @@ public class Debater {
 		if(blocks.length == 2)
 			last = blocks[1];
 		else if(blocks.length == 3) {
-			if(blocks[2].endsWith(".") || blocks[2].length() == 2 || blocks[2].matches("III|IV|V|VI|VII|VIII|IX|X")) {
+			if(blocks[2].endsWith(".") || blocks[2].length() == 2 || blocks[2].matches("III|IV|V|VI|VII|VIII|IX|X|Junior")) {
 				last = blocks[1];
 				surname = blocks[2];
 			}
@@ -42,55 +45,37 @@ public class Debater {
 	}
 	
 	public boolean equals(Debater debater) {
+		boolean replaceThis = false;
 		String first = debater.getFirst();
 		String last = debater.getLast();
 		String school = debater.getSchool();
 		if(school != null && this.school != null) { // TODO: Make this not look terrible
 			
-			boolean failed1 = false;
-			boolean failed2 = false;
-			
-			String[] blocks1 = SQLHelper.cleanString(this.school).split(" ");
-			String[] blocks2 = SQLHelper.cleanString(school).split(" ");
-			if(blocks1.length > blocks2.length) {
-				String[] temp = blocks2;
-				blocks2 = blocks1;
-				blocks1 = temp;
-			}
-			boolean found = false;
-			for(int i = 0;i<blocks2.length;i++) {
-				for(int k = 0;k<blocks1.length;k++)
-					if(blocks2[i].equals(blocks1[k]))
-						found = true;
-				if(!found) {
-					failed1 = true;
-					break;
-				}
-				found = false;
+			ArrayList<String> compare = new ArrayList<String>(Arrays.asList(SQLHelper.cleanString(this.school).split(" ")));
+			ArrayList<String> against = new ArrayList<String>(Arrays.asList(SQLHelper.cleanString(school).split(" ")));
+			if(SQLHelper.cleanString(this.school).split(" ").length < SQLHelper.cleanString(school).length()) {
+				compare = new ArrayList<String>(Arrays.asList(SQLHelper.cleanString(school).split(" ")));
+				against = new ArrayList<String>(Arrays.asList(SQLHelper.cleanString(this.school).split(" ")));
 			}
 			
-			// Reverse
-			String[] temp = blocks2;
-			blocks2 = blocks1;
-			blocks1 = temp;
-			found = false;
-			for(int i = 0;i<blocks2.length;i++) {
-				for(int k = 0;k<blocks1.length;k++)
-					if(blocks2[i].equals(blocks1[k]))
-						found = true;
-				if(!found) {
-					failed2 = true;
-					break;
-				}
-				found = false;
-			}
-			
-			if(failed1 && failed2)
+			if(!against.containsAll(compare))
 				return false;
+			else
+				replaceThis = true;
 			
 		}
-		if(((this.first == null && first == null) || SQLHelper.cleanString(this.first).equals(SQLHelper.cleanString(first))) &&
-				((this.last == null && last == null) || SQLHelper.cleanString(this.last).equals(SQLHelper.cleanString(last)))) {
+		else if(this.school == null && school != null)
+			replaceThis = true;
+		
+		if((SQLHelper.cleanString(this.first).equals(SQLHelper.cleanString(first))) &&
+				(SQLHelper.cleanString(this.last).equals(SQLHelper.cleanString(last)))) {
+			if(replaceThis) {
+				this.first = debater.getFirst();
+				this.middle = debater.getMiddle();
+				this.last = debater.getLast();
+				this.surname = debater.getSurname();
+				this.school = debater.getSchool();
+			}
 			return true;
 		}
 		return false;
