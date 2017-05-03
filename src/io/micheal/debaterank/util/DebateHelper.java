8c8cf18084f7364c2503ab97faabc2001802ea50
Micheal <micheal.myers@hotmail.com>
2017-05-02 20:20:43 -0500

    Added update team IDs

--- src/io/micheal/debaterank/util/DebateHelper.java
+++ src/io/micheal/debaterank/util/DebateHelper.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.Level;
 import org.jsoup.nodes.Document;
 
 import io.micheal.debaterank.Debater;
+import io.micheal.debaterank.Team;
 import io.micheal.debaterank.UnsupportedNameException;
 
 public class DebateHelper {
@@ -24,14 +24,14 @@ public class DebateHelper {
 	 * @throws SQLException 
 	 * @throws UnsupportedNameException 
 	 */
-	public static int getDebaterID(SQLHelper sql, Debater debater) throws SQLException, UnsupportedNameException {
+	public static int getDebaterID(SQLHelper sql, Debater debater) throws SQLException {
 		if(debater.getID() != null)
 			return debater.getID();
-		ResultSet index = sql.executeQueryPreparedStatement("SELECT id, first, last, school FROM debaters WHERE first_clean<=>? AND last_clean<=>?", SQLHelper.cleanString(debater.getFirst()), SQLHelper.cleanString(debater.getLast()));
+		ResultSet index = sql.executeQueryPreparedStatement("SELECT id, school FROM debaters WHERE first_clean<=>? AND middle_clean<=>? AND last_clean<=>? AND surname_clean<=>?", SQLHelper.cleanString(debater.getFirst()), SQLHelper.cleanString(debater.getMiddle()), SQLHelper.cleanString(debater.getLast()), SQLHelper.cleanString(debater.getSurname()));
 		if(index.next()) {
 			do {
-				Debater d = new Debater(index.getString(2) + " " + index.getString(3), index.getString(4));
-				if(debater.equals(d)) {
+				Debater clone = new Debater(debater.getFirst(), debater.getMiddle(), debater.getLast(), debater.getSurname(), index.getString(2));
+				if(debater.equals(clone)) {
 					int ret = index.getInt(1);
 					index.close();
 					return ret;
@@ -42,20 +42,32 @@ public class DebateHelper {
 		return sql.executePreparedStatementArgs("INSERT INTO debaters (first, middle, last, surname, school, first_clean, middle_clean, last_clean, surname_clean, school_clean) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", debater.getFirst(), debater.getMiddle(), debater.getLast(), debater.getSurname(), debater.getSchool(), SQLHelper.cleanString(debater.getFirst()), SQLHelper.cleanString(debater.getMiddle()), SQLHelper.cleanString(debater.getLast()), SQLHelper.cleanString(debater.getSurname()), SQLHelper.cleanString(debater.getSchool()));
 	}
 	
-	public static void updateDebaterIDs(SQLHelper sql, HashMap<String, Debater> competitors) throws SQLException {
-		for(Map.Entry<String, Debater> entry : competitors.entrySet()) {
-			try {
-				entry.getValue().setID(DebateHelper.getDebaterID(sql, entry.getValue()));
-			} catch (UnsupportedNameException une) {}
+	/**
+	 * Searches the SQL tables for the specified team, returning the <b>first</b> result. If no match is found, a debater will be created and returned
+	 * @return
+	 * @throws SQLException 
+	 * @throws UnsupportedNameException 
+	 */
+	public static int getTeamID(SQLHelper sql, Team team, String event) throws SQLException {
+		if(team.getID() != null)
+			return team.getID();
+		int left = getDebaterID(sql, team.getLeft());
+		int right = getDebaterID(sql, team.getRight());
+		ResultSet index = sql.executeQueryPreparedStatement("SELECT id FROM teams WHERE ((debater1=? AND debater2=?) OR (debater2=? AND debater1=?)) AND event<=>?", left, right, left, right, event);
+		if(index.next()) {
+			int ret = index.getInt(1);
+			index.close();
+			return ret; // Should only return one team
 		}
+		return sql.executePreparedStatementArgs("INSERT INTO teams (debater1, debater2, event) VALUES (?, ?, ?)", left, right, event);
 	}
 	
-	public static void updateTeamIDs(SQLHelper sql, HashMap<String, Pair<Debater, Debater>> competitors) throws SQLException {
-		for(Map.Entry<String, Pair<Debater, Debater>> entry : competitors.entrySet()) {
-			try {
-				entry.getValue().getLeft().setID(getDebaterID(sql, entry.getValue().getLeft()));
-				entry.getValue().getRight().setID(getDebaterID(sql, entry.getValue().getRight()));
-			} catch (UnsupportedNameException une) {}
+	public static void updateTeamIDs(SQLHelper sql, HashMap<String, Team> competitors, String event) throws SQLException {
+		for(Map.Entry<String, Team> entry : competitors.entrySet()) {
+			entry.getValue().getLeft().setID(getDebaterID(sql, entry.getValue().getLeft())); // In case IDs are not set
+			entry.getValue().getRight().setID(getDebaterID(sql, entry.getValue().getRight()));
+			
+			entry.getValue().setID(getTeamID(sql, entry.getValue(), event));
 		}
 	}
 	
