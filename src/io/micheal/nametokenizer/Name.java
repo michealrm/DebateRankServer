7b9546e477296afca2cfa4cc67ab9aa0c39885fb
Micheal <micheal.myers@hotmail.com>
2017-05-15 15:07:36 -0500

    Start on name tokenizer

---
+++ src/io/micheal/nametokenizer/Name.java
@@ -0,0 +1,8 @@
+package io.micheal.nametokenizer;
+
+public enum Name {
+    FIRST,
+    MIDDLE,
+    LAST,
+    SURNAME;
+}
---
+++ src/io/micheal/nametokenizer/Tokenizer.java
@@ -0,0 +1,46 @@
+package io.micheal.nametokenizer;
+
+import static io.micheal.nametokenizer.Name.*;
+import java.util.HashMap;
+
+public class Tokenizer {
+
+    public static HashMap<Name, String> tokenize(String name) {
+        return tokenize(name, LAST);
+    }
+
+    public static HashMap<Name, String> tokenize(String name, Name priority) {
+        HashMap<Name, String> tokenized = new HashMap<Name, String>();
+        if(name == null || name.equals(""))
+            return tokenized;
+        String[] split = name.split("\\s+");
+        if(split.length == 1)
+            tokenized.put(priority, split[0]);
+        else if(split.length == 2) {
+            tokenized.put(FIRST, split[0]);
+            tokenized.put(LAST, split[1]);
+        }
+        else if(split.length == 3) {
+            if(split[2].contains("Jr|Junior|III|IV|V|VI|VII|VIII|IX|X")) {
+                tokenized.put(FIRST, split[0]);
+                tokenized.put(LAST, split[1]);
+                tokenized.put(SURNAME, split[2]);
+            }
+            else {
+                tokenized.put(FIRST, split[0]);
+                tokenized.put(MIDDLE, split[1]);
+                tokenized.put(LAST, split[2]);
+            }
+        }
+        else if(split.length == 4) {
+            if(split[3].contains("Jr|Junior|III|IV|V|VI|VII|VIII|IX|X")) {
+                tokenized.put(FIRST, split[0]);
+                tokenized.put(MIDDLE, split[1]);
+                tokenized.put(LAST, split[2]);
+                tokenized.put(SURNAME, split[3]);
+            }
+        }
+        return null;
+    }
+
+}
