235cb461459257930b3f5702d9e0374b41b8c145
Aspecting <mrmyers2k@gmail.com>
2017-07-21 10:58:58 -0500

    RatingsHelper

---
+++ src/io/micheal/debaterank/util/RatingsHelper.java
@@ -0,0 +1,12 @@
+package io.micheal.debaterank.util;
+
+import javafx.util.Pair;
+import org.joda.time.DateTime;
+
+public class RatingsHelper {
+
+	public Pair<DateTime, DateTime> getNearestDateRange() {
+		return null;
+	}
+
+}
